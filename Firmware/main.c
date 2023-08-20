213c14a83a8aa59d586fa1df2062ec4f53c92e4e
Rubens Pirie <rupirie@Ru-Firmware.localdomain>
2023-08-21 00:20:31 +0100

    Added some c

--- Firmware/main.c
+++ Firmware/main.c
@@ -27,18 +27,22 @@ int main() {
 
 
     bool val = false;
-    int x = 0;
+
     while(true)  
     {  
+        set_char(0, 'A', true);
         gpio_put(25, val);
         val = !val;
-       	// clock_read_time();
+        set_char(0, 'B', true);
+       	clock_read_time();
+        set_char(0, 'C', true);
 
         // Clock has been reset (MUST BE SET)
 		if(clock_buffer[6]==0x00 && clock_buffer[5]==0x01 && clock_buffer[4]==0x01)
 		{
 			clock_set_time(); 
 		}
+        set_char(0, 'D', true);
 
 
 		clock_buffer[0] = clock_buffer[0] & 0x7F; //sec
@@ -54,11 +58,16 @@ int main() {
 		//year/month/day
 		// sprintf(clock_string_buffer, "20%02x/%02x/%02x  ",clock_buffer[6],clock_buffer[5],clock_buffer[4]);
 		//hour:minute/second
+        set_char(0, 'E', true);
 		sprintf(clock_string_buffer, "%02x:%02x:%02x  ",clock_buffer[2],clock_buffer[1],clock_buffer[0]);
+        set_char(0, 'F', true);
+
 		//weekday
 		// sprintf(clock_string_buffer, "%s\n",week[(unsigned char)clock_buffer[3]-1]);
 
         display_string(clock_string_buffer);
-        sleep_ms(15);
+        sleep_ms(100);
 	}  
+
+    set_char(0, 'Z', true);
 }
